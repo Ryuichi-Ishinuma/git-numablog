be967c06229973a62ed94445158ab28f90364298
Ryuichi Ishinuma <doronuma0422@gmail.com>
2020-04-04 00:27:59 +0900

    gulpのsassコンパイル記述追加 cssをscssにモジュールごとに分割 徐々に作業を進めていく。 一旦はcssのコードは残しつつ、sassファイルにペーストしてファイルを分割していく。

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,23 @@
 const gulp = require("gulp");
 const sass = require("gulp-sass");
 
+// Sassファイルのコンパイル
 gulp.task("sass", function() {
   return (
     gulp.src('./src/sass/*.scss')
-    .pipe(sass())
-    .pipe(gulp.dest('./dist/css'))
+    .pipe(sass({
+      outputStyle: "expanded"
+    }))
+    .pipe(gulp.dest('./dist/css/*.css'))
   );
 });
+
+// 推奨仕様の記述  実行できないので一旦コメントアウト
+// const compileSass = () =>
+//     src('./src/sass/*.scss')
+//     .pipe(sass({
+//       outputStyle: "expanded"
+//     }))
+//     .pipe(gulp.dest('./dist/css/*.css'))
+// const watchSassFiles = () => watch('./src/sass/*.scss', compileSass);
+// exports.default = watchSassFiles;
