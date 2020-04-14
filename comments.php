df3e40a0b982b7601e1bb34d03a485b391004202
Ryuichi Ishinuma <doronuma0422@gmail.com>
2020-04-14 19:26:12 +0900

    諸々デザイン修正 コメント欄のデザイン修正 シングルページのマージン等調整 目次のマージン調整etc

--- comments.php
+++ comments.php
@@ -1,15 +1,11 @@
-<div id="comments">
+<div class="comments">
 <?php if( have_comments() ): ?>
-<h3>コメント</h3>
+<h2>コメント</h2>
 
 <ul>
 <?php wp_list_comments( 'avatar_size=0&max_depth=1&format=html5'); ?>
 </ul>
 <?php endif; ?>
 
-<?php comment_form( 'format=html5' ); ?>
+<?php comment_form( 'format=html5&title_reply=コメント&label_submit=Submit' ); ?>
 </div>
-
-
-
-
